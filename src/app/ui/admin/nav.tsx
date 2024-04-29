be76a7eeffda19d144532727a5d54bf7f32ba872
Salisa <salisa@swtech.co.th>
2024-04-29 23:44:46 +0700

    feat: admin navbar link to page

--- src/app/ui/admin/nav.tsx
+++ src/app/ui/admin/nav.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Nav() {
   return (
@@ -6,7 +7,7 @@ export default function Nav() {
       <div className="font-bold text-[40px] ml-6">Admin</div>
       <div className="flex flex-col justify-between h-full">
         <div>
-          <div className="flex py-4 px-2 gap-2">
+          <Link href="/admin" className="flex py-4 px-2 gap-2">
             <Image
               src="/icon/home.svg"
               width={24}
@@ -14,8 +15,8 @@ export default function Nav() {
               alt="home icon"
             />
             <div>Home</div>
-          </div>
-          <div className="flex py-4 px-2 gap-2">
+          </Link>
+          <Link href="/admin/history" className="flex py-4 px-2 gap-2">
             <Image
               src="/icon/history.svg"
               width={24}
@@ -23,8 +24,8 @@ export default function Nav() {
               alt="history icon"
             />
             <div>History</div>
-          </div>
-          <div className="flex py-4 px-2 gap-2">
+          </Link>
+          <Link href="/user" className="flex py-4 px-2 gap-2">
             <Image
               src="/icon/switch.svg"
               width={24}
@@ -32,7 +33,7 @@ export default function Nav() {
               alt="switch icon"
             />
             <div>Switch to user</div>
-          </div>
+          </Link>
         </div>
         <div className="flex py-4 px-2 gap-2">
           <Image
